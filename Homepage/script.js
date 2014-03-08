a83fa7905eeabd8917b6afd456d2d666ac3d5f7b
Florian Madar <madarflorin@gmail.com>
2014-03-08 19:29:11 +0200

    Project

--- Homepage/script.js
+++ Homepage/script.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
+	$("#frame1").attr("src","http://ts3.travian.ro/dorf1.php?newdid=79715&");
+});	
+
+
+$(document).ready(function() {
 $("#frame_load").click(function(){
 	$("#frame1").attr("src","body/stuff.html");
-})	
-})
+});
+});
 
+$(document).ready(function() {
+$("#back_home").click(function(){
+	$("#frame1").attr("src","http://ts3.travian.ro/dorf1.php?newdid=79715&");
+});	
+});
 
 
