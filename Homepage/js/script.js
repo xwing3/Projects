ea251d5d55f2e88a57328150c9fb7f680f9a2e00
Florian Madar <madarflorin@gmail.com>
2014-03-12 23:31:53 +0200

    update

--- Homepage/js/script.js
+++ Homepage/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 $("#frame_load").click(function(){
-	$("#frame1").attr("src","body/stuff.html");
+	$("#load").load("body/stuff.html");
 });
 });
 
