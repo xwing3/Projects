0ba8bc4cdd582d4930791127644caa977b147571
Florian Madar <madarflorin@gmail.com>
2014-03-15 21:29:53 +0200

    update

--- Homepage/js/script.js
+++ Homepage/js/script.js
@@ -1,3 +1,31 @@
+
+/*
+Copyright (c) 2014 Florian Madar
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+
+
+// page loading funtions
+
 $(document).ready(function(){
 	$("#load").load("body/home.html")
 });
@@ -10,9 +38,11 @@ $(document).on("click", "#frame_load1", function(){
 $(document).ready(function() {
 $("#frame_load").click(function(){
 	$("#load").load("body/stuff.html");
-});
+	});
 });
 
+
+//footer social media styling
 
 $(document).ready(function(){
 	$("#img1").mouseover(function(){
