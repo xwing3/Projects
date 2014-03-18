08cc1a455be8969d927b8d4caf63ed8e0aeadc80
Florian Madar <madarflorin@gmail.com>
2014-03-18 21:52:16 +0200

    homepage v2 using bootstrap

---
+++ Homepage v2/js/script.js
@@ -0,0 +1,39 @@
+
+/*
+Copyright (c) 2014 Florian Madar
+
+Licensed under the MIT license
+https://github.com/xwing3/Projects/blob/master/LICENSE.md
+*/
+
+
+
+
+// page loading funtions
+
+$(document).ready(function(){
+	$(".jumbotron").load("body/home.html")
+});
+
+
+$(document).on("click", "#frame_load1", function(){
+	$(".jumbotron").load("body/home.html")
+});
+
+$(document).ready(function() {
+$("#frame_load").click(function(){
+	$(".jumbotron").load("body/stuff.html");
+	});
+});
+
+// adding .active
+
+$(document).on("click", "#frame_load", function(){
+	$(this).addClass("active");
+	$("#frame_load1").removeClass("active");
+});
+
+$(document).on("click", "#frame_load1", function(){
+	$(this).addClass("active");
+	$("#frame_load").removeClass("active");
+});
