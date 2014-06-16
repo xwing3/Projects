698dfce9d9e998a753df337afa35a17ada91f570
xwing3 <madarflorin@gmail.com>
2014-06-16 16:16:56 +0300

    update

--- Template v1/js/contact.js
+++ Template v1/js/contact.js
@@ -4,39 +4,16 @@
 
 
     var sub = document.getElementById("submit");
-    var first = document.getElementById("name_f");
-    var last = document.getElementById("name_l");
-    var email = document.getElementById("exampleInputEmail1");
-    var mess = document.getElementById("subject");
+    var first = document.getElementsByClassName("input-lg");
 
         sub.onclick = function() {
 
-            if (first.value == "" || first.value == null) {
-
-                $(".alert").removeClass("hide");
-
-
-        } else {
-                $(".alert").addClass("hide");
-                alert("thank you for giving us your first name");
-        }
-            if (last.value == "" || last.value == null) {
-
-            } else {
-                alert("thank you for giving us your last name");
-            }
-
-            if (email.value == "" || email.value == null) {
-
-            } else {
-                alert("thank you for giving us your email");
-            }
-
-            if (mess.value == "" || mess.value == null) {
-
-            } else {
-                alert("thank you typing you message");
-            }
+            $("input").each(function() {
+                if(!$.trim(this.value).length) { 
+                    $(".alert").removeClass('hide');
+                } else
+                    $(".alert").addClass('hide');
+            });
 
     };
 
