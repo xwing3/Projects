0907bbdb5a17e492f397c8e50e9536ae85e5c03b
Florian Madar <madarflorin@gmail.com>
2014-06-16 22:55:22 +0300

    update

--- Template v1/js/contact.js
+++ Template v1/js/contact.js
@@ -4,16 +4,55 @@
 
 
     var sub = document.getElementById("submit");
-    var first = document.getElementsByClassName("input-lg");
+    var first = document.getElementById("name_f");
+    var first2 = document.getElementById("name_l");
+    var first3 = document.getElementById("yourEmail");
+    var first4 = document.getElementById("subject");
+    var first5 = document.getElementById("message");
 
-        sub.onclick = function() {
+      function validate() {
+            if(!$.trim(first.value).length) {
+                     $(".alert").removeClass("hide");
+                        return false;
+                        } else {
+                            $(".alert").addClass("hide");
+                     }
+          if(!$.trim(first2.value).length) {
+              $(".alert").removeClass("hide");
+                return false;
+          } else {
+              $(".alert").addClass("hide");
 
-            $("input").each(function() {
-                if(!$.trim(this.value).length) { 
-                    $(".alert").removeClass('hide');
-                } else
-                    $(".alert").addClass('hide');
-            });
+          }
+          if(!$.trim(first3.value).length) {
+              $(".alert").removeClass("hide");
+              return false;
+          } else {
+              $(".alert").addClass("hide");
 
-    };
+          }
+          if(!$.trim(first4.value).length) {
+              $(".alert").removeClass("hide");
+              return false;
+          } else {
+              $(".alert").addClass("hide");
+
+          }
+          if(!$.trim(first5.value).length) {
+              $(".alert").removeClass("hide");
+              return false;
+          } else {
+              $(".alert").addClass("hide");
+
+          }
+       }
+
+
+    $(sub).click(function () {
+        validate();
+
+    });
+
+
+
 
